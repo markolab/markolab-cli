a0b49d6c6859d06857e61f32936965cf27dd4c04
Jeffrey Markowitz <jeffrey.markowitz@bme.gatech.edu>
2023-07-23 23:52:43 +0000

    Minor changes

--- src/markolab_cli/cli.py
+++ src/markolab_cli/cli.py
@@ -1,5 +1,7 @@
 import click
-from markolab_cli.io import convert_raw_to_avi
+import functools
+from tqdm.auto import tqdm
+from markolab_cli.io import convert_dat_to_avi
 
 
 @click.group()
@@ -7,18 +9,95 @@ def cli():
     pass
 
 
-@cli.command(name="convert-raw-to-avi", context_settings={"show_default": True})
+def convert_params(func):
+    @click.option("-b", "--chunk-size", type=int, default=3000, help="Chunk size")
+    @click.option(
+        "--delete", type=bool, is_flag=True, help="Delete raw file if encoding is sucessful"
+    )
+    @click.option("-t", "--threads", type=int, default=6, help="Number of threads for encoding")
+    @click.option("-c", "--codec", type=str, default="ffv1", help="ffmpeg codec")
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@cli.command(name="convert-dat-to-avi", context_settings={"show_default": True})
 @click.argument("input-filename", type=click.Path(exists=True, resolve_path=True))
 @click.option(
     "-o", "--output-filename", type=click.Path(), default=None, help="Path to output file"
 )
-@click.option("-b", "--chunk-size", type=int, default=3000, help="Chunk size")
-@click.option("--delete", type=bool, is_flag=True, help="Delete raw file if encoding is sucessful")
-@click.option("-t", "--threads", type=int, default=6, help="Number of threads for encoding")
-@click.option("-c", type=str, default="ffv1", help="ffmpeg codec")
-def convert_raw_to_avi_cli(input_filename, output_filename, chunk_size, delete, threads, codec):
-    convert_raw_to_avi(input_filename, output_filename, chunk_size, delete, threads, codec)
+@convert_params
+def convert_dat_to_avi_cli(input_filename, output_filename, chunk_size, delete, threads, codec):
+    convert_dat_to_avi(
+        input_filename=input_filename,
+        output_filename=output_filename,
+        chunk_size=chunk_size,
+        delete=delete,
+        threads=threads,
+        codec=codec,
+    )
 
 
-if __name__ == '__main__':
+@cli.command(name="convert-dat-to-avi-batch", context_settings={"show_default": True})
+@click.option("-d", "--chk-dir", type=click.Path(), default=None, help="Directory to check")
+@click.option("-f", "--file-filter", type=str, default="*.dat", help="File filter")
+@convert_params
+def convert_dat_to_avi_cli_batch(chk_dir, file_filter, chunk_size, delete, threads, codec):
+    import os
+    import glob
+
+    if chk_dir is None:
+        chk_dir = os.getcwd()
+
+    files_proc = glob.glob(os.path.join(chk_dir, "**", file_filter), recursive=True)
+    for f in tqdm(files_proc, desc="Total files"):
+        convert_dat_to_avi(
+            input_filename=f,
+            output_filename=None,
+            chunk_size=chunk_size,
+            delete=delete,
+            threads=threads,
+            codec=codec,
+        )
+
+
+@cli.command(
+    name="create-slurm-batch",
+    context_settings={"show_default": True, "auto_envvar_prefix": "MARKOLABCLI_BATCH"},
+)
+@click.argument("command", type=str)
+@click.option("-d", "--chk-dir", type=click.Path(), default=None, help="Directory to check")
+@click.option("-f", "--file-filter", type=str, default="*.dat", help="File filter", show_envvar=True)
+@click.option("--ncpus", "-n", type=int, default=2, help="Number of CPUs", show_envvar=True)
+@click.option("--memory", "-m", type=str, default="10GB", help="RAM string", show_envvar=True)
+@click.option("--wall-time", "-w", type=str, default="3:00:00", help="Wall time", show_envvar=True)
+@click.option("--partition", type=str, default="short", help="Partition name", show_envvar=True)
+@click.option("--prefix", type=str, default=None, help="Command prefix", show_envvar=True)
+@click.option("--account", type=str, default=None, help="Account name", show_envvar=True)
+def create_slurm_batch_cli(
+    command, chk_dir, file_filter, ncpus, memory, wall_time, partition, prefix, account
+):
+    import os
+    import glob
+
+    if chk_dir is None:
+        chk_dir = os.getcwd()
+
+    files_proc = glob.glob(os.path.join(chk_dir, "**", file_filter), recursive=True)
+
+    if prefix is not None:
+        base_command = f"{prefix};"
+    else:
+        base_command = ""
+
+    cluster_prefix = f'sbatch -n {ncpus:d} --mem={memory} -p {partition} -t {wall_time} -A {account} --wrap "'
+    issue_command = f"{cluster_prefix}{base_command}"
+    for f in files_proc:
+        run_command = f'{issue_command}{command} {f}"'
+        print(run_command)
+
+
+if __name__ == "__main__":
     cli()
--- src/markolab_cli/io.py
+++ src/markolab_cli/io.py
@@ -6,15 +6,16 @@ from markovids.vid import io
 from typing import Optional
 
 
-def convert_raw_to_avi(
+def convert_dat_to_avi(
     input_filename: str,
     output_filename: Optional[str] = None,
     chunk_size: int = 3000,
     delete: bool = False,
     threads: int = 8,
     codec: str = "ffv1",
-):
-    base_filename = os.path.splitext(os.path.basename(input_filename))[0]
+) -> None:
+    base_filename = os.path.splitext(input_filename)[0]
+    camera_name = os.path.basename(base_filename)
     tstamp_filename = f"{base_filename}.txt"
     metadata_filename = os.path.join(os.path.dirname(input_filename), "metadata.toml")
 
@@ -23,13 +24,17 @@ def convert_raw_to_avi(
             os.path.dirname(input_filename), "{}.avi".format(base_filename)
         )
 
-    metadata = toml.load(metadata_filename)["camera_metadata"][base_filename]
+    try:
+        metadata = toml.load(metadata_filename)["camera_metadata"][camera_name]
+    except KeyError:
+        print(f"Could not locate metadata for {camera_name}")
+        return None
     frame_size = metadata["Width"], metadata["Height"]
 
     try:
         fps = int(np.round(metadata["AcquisitionFrameRate"]))
     except KeyError:
-        timestamps = io.read_timestamps(f"{tstamp_filename}.txt")
+        timestamps = io.read_timestamps(tstamp_filename)
         fps = int(np.round((1 / timestamps["device_timestamp"].diff()).median()))
 
     dtype = io.pixel_format_to_np_dtype(metadata["PixelFormat"])
@@ -50,7 +55,7 @@ def convert_raw_to_avi(
         output_obj = io.AviWriter(
             filepath=output_filename,
             frame_size=frame_size,
-            dtype=dtype,  # check this...
+            dtype=dtype,  # type: ignore
             pixel_format=pixel_format,
             threads=threads,
             codec=codec,
@@ -71,11 +76,14 @@ def convert_raw_to_avi(
         raw_frames = input_obj.get_frames(batch)
         encoded_frames = new_input_obj.get_frames(batch)
 
-        if not np.array_equal(raw_frames, encoded_frames):
+        if not np.array_equal(raw_frames, encoded_frames): # type: ignore
             raise RuntimeError(
                 "Raw frames and encoded frames not equal from {} to {}".format(batch[0], batch[-1])
             )
 
+    input_obj.close()
+    new_input_obj.close()
+    
     print("Encoding successful")
     if delete:
         print("Deleting {}".format(input_filename))
